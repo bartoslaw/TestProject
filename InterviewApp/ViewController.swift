dcad3b08c5f2a997596c0fd80ba83bc5cd99ab52
bart <bs.smektala@gmail.com>
2020-12-16 18:40:30 +0100

    Initial Commit

---
+++ InterviewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterviewApp
+//
+//  Created by Bartłomiej Smektała on 16/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
